a4354f695ee29cd8afc738ec812ad7b4978bae7b
Sreenath <sreenathsrp256@gmail.com>
2025-04-18 16:34:47 +0530

    Changed the base url

--- src/constents/Api.ts
+++ src/constents/Api.ts
@@ -1,7 +1,7 @@
-// export const vars={
-//     api_url:"https://ztartserver.dostudio.info"
-// }
-
 export const vars={
-    api_url:"http://localhost:3000"
+    api_url:"https://ztartserver.dostudio.info"
 }
+
+// export const vars={
+//     api_url:"http://localhost:3000"
+// }
